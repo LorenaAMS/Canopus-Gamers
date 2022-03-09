5b267974dd60c9a8176b460948347d59a4066825
LorenaAMS <101295444+LorenaAMS@users.noreply.github.com>
2022-03-09 17:49:10 -0500

    Initial commit

---
+++ contacto.js
@@ -0,0 +1,8 @@
+function boton(){
+    const formContact = document.getElementById("form-contact");
+    const boton = document.getElementById("boton");
+    btnContacto.addEventListener("submit", (e) => {
+    e.boton.value;
+    console.log("click");
+    });
+};
